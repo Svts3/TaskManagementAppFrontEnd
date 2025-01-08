cf9e0243a2a054dca6f96f2d8936dbf2a5a6c27b
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2025-01-09 00:34:46 +0200

    Add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="nav-buttons">
+                <button className="button">Sign up</button>
+                <button className="button">Sign in</button>
+                <button className="button">Profile</button>
+            </div>
+
+        </header>
+
+    )
+}
