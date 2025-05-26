40155c7447c9558935f090570fa8bc4d7f168e80
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2025-05-26 23:10:11 +0300

    Add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,29 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router'
-import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom"; // краще одразу імпортувати повний пакет
+import { Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import SignUp from "./components/SignUp/SignUp";
 import SignIn from "./components/SignIn/SignIn";
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="sign-up" element={<SignUp/>}/>
-      <Route path="sign-in" element={<SignIn/>}/>
-    </Routes>
-  </BrowserRouter>
-)
+import UserWorkspaces from "./components/UserWorkspaces/UserWorkspaces";
+import WorkspacePage from "./components/WorkspacePage/WorkspacePage";
+import { Navigate } from "react-router-dom";
+import Header from "./components/Header/Header";
+import UserDetailsPage from "./components/UserProfilePage/UserProfilePage";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Navigate to="/sign-in" replace />} />
+        <Route path="sign-up" element={<><Header/><SignUp /></>} />
+        <Route path="sign-in" element={<><Header/><SignIn /></>} />
+        <Route path="workspaces" element={<><Header/><UserWorkspaces /></>} />
+        <Route path="workspace/:id" element={<><Header/><WorkspacePage /></>} />
+        <Route path="profile/:id" element={<><Header/><UserDetailsPage /></>} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
+);
